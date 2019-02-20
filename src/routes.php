7ba89e0305dc82570052137648f92e34b426ed9a
Dezodev <dezodev@gmail.com>
2019-02-20 16:35:44 +0100

    Init Slim framework

---
+++ src/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Routes
+$app->get('/', function (Request $request, Response $response, array $args) {
+    // Render index view
+    return $response->withJson([ 'status' => 'ok' ]);
+});
